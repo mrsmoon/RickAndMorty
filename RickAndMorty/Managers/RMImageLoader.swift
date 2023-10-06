db7d20b26023a870696766f4202631c752e3cce8
Sera <dev.sera20@gmail.com>
2023-10-05 17:01:20 -0700

    Added image caching.

---
+++ RickAndMorty/Managers/RMImageLoader.swift
@@ -0,0 +1,42 @@
+//
+//  RMImageLoader.swift
+//  RickAndMorty
+//
+//  Created by Seher Aytekin on 10/5/23.
+//
+
+import Foundation
+
+final class RMImageLoader {
+    static let shared = RMImageLoader()
+    
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
+    public init() {}
+    
+    /// Get imagee content with URL
+    /// - Parameters:
+    ///   - url: Source URL
+    ///   - completion: Callback
+    public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            
+            let key = url.absoluteString as NSString
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
